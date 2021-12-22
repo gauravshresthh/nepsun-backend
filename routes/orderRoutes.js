274d2226bf9332facf4e0524e396401e5f971127
Gaurav Shrestha <gauravshresthh@gmail.com>
2021-12-22 14:20:17 +0545

    order router implemented

---
+++ routes/orderRoutes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const orderController = require('../controllers/orderController');
+
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router
+	.route('/')
+	.get(
+		authController.protect,
+		authController.permit('admin'),
+		orderController.getAllOrders
+	)
+	.post(
+		authController.protect,
+		authController.permit('user'),
+		orderController.createOrder
+	);
+
+router
+	.route('/:id')
+	.get(
+		authController.protect,
+		authController.permit('admin'),
+		orderController.getOrder
+	)
+	.put(
+		authController.protect,
+		authController.permit('admin'),
+		orderController.updateOrder
+	);
+
+module.exports = router;
