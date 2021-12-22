5efea9654462d5b865102986be4e5780d2691c94
Gaurav Shrestha <gauravshresthh@gmail.com>
2021-12-22 17:42:05 +0545

    testing

--- utils/sendEmailtest.js
+++ utils/sendEmailtest.js
@@ -1,34 +1,8 @@
-const nodemailer = require('nodemailer');
+const sendEmail = require('./sendEmail');
 
-const sendEmail = async options => {
-	// 1) Create a transporter
-	const transporter = nodemailer.createTransport({
-		// host: process.env.EMAIL_HOST,
-		// port: process.env.EMAIL_PORT,
-		service: 'gmail',
-		auth: {
-			user: 'gauravshresthh@gmail.com',
-			pass: 'pikachhu@9816947062',
-		},
-	});
-
-	// 2) Define the email options
-	const mailOptions = {
-		from: 'Hamro Service <noreply@hamroservice.com>',
-		to: 'gaurav.shrestha@treeleaf.ai',
-		subject: options.subject,
-		text: options.message,
-		// html:
-	};
-
-	// 3) Actually send the email
-	try {
-		await transporter.sendMail(mailOptions);
-		console.log("mail sent")
-	} catch (error) {
-		console.log(error);
-	}
+const options = {
+	email: 'gaurav.shrestha@treeleaf.ai',
+	subject: 'this is a test',
+	text: 'hey there',
 };
-const options = { subject: 'this is a test', text: 'hey there' };
 sendEmail(options);
-module.exports = sendEmail;
