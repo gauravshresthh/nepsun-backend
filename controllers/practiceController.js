493c56b09f3b71a688889c2388ec3dd8079898d7
Sresta Malla <mallasresta@gmail.com>
2023-02-02 18:08:57 +0545

    changed global username and email

--- controllers/practiceController.js
+++ controllers/practiceController.js
@@ -1,6 +1,6 @@
 exports.getPractice = (req, res, next) => {
   const welcomeText =
-    "Welcome to Hamro Service API, This is an API for Hamro service multi-platform software. This is a practice API. changed known host, changed known host as well as ssh key";
+    "Welcome to Hamro Service API, This is an API for Hamro service multi-platform software. This is a practice API. changed global email and username";
 
   return res.status(200).json({ status: "success", data: welcomeText });
 };
